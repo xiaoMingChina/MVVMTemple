e0ce483e04de77e9e43c17d0c317aa9f319baf08
朱丽明 <385208272@qq.com>
2022-05-09 02:27:54 +0800

    init

---
+++ mvvmTemple/mvvmTemple/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mvvmTemple
+//
+//  Created by Ming on 2022/5/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
